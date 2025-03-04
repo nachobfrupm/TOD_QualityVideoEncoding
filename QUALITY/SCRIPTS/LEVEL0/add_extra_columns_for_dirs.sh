de47fdcdafa6328718e26f6373a1784ac81cf972
Benito Frontelo Ignacio <ignacio.benito_frontelo@nokia.com>
2025-03-04 18:11:00 +0100

    Removed folder LEVEL0 from repository

--- QUALITY/SCRIPTS/LEVEL0/add_extra_columns_for_dirs.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-ROOT_DIR=/media/xruser/REMOTEDRIVING/TOD/TESIS/QUALITY2.0
-for dir in $(find /media/xruser/REMOTEDRIVING/TOD/TESIS/QUALITY2.0/INPUT/ARGODRIVE/DONE_SEQUENCES -type d -name 'SEQUENCE_*')
-do 
-echo $dir
-${ROOT_DIR}/SCRIPTS/LEVEL0/add_extra_yolov_columns.sh $dir
-done
-
--- QUALITY/SCRIPTS/LEVEL0/process_argo_drive_sequences.sh
+++
@@ -1,8 +0,0 @@
-for direc in $(ls /media/xruser/REMOTEDRIVING/TOD/TESIS/QUALITY2.0/INPUT/ARGODRIVE/SEQUENCES/|grep -v _01|grep -v _02|grep -v _03|grep -v _04)
-do 
-echo $direc
-echo "./generate_it_all.sh /media/xruser/REMOTEDRIVING/TOD/TESIS/QUALITY2.0/INPUT/ARGODRIVE/SEQUENCES/${direc} mp4"
-./generate_it_all.sh /media/xruser/REMOTEDRIVING/TOD/TESIS/QUALITY2.0/INPUT/ARGODRIVE/SEQUENCES/${direc} mp4
-
-cd /media/xruser/REMOTEDRIVING/TOD/TESIS/QUALITY2.0/SCRIPTS/LEVEL0
-done
